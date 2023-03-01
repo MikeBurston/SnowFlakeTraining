5efc7c81f67f2759c422ca8e40954565cf766028
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-01 12:21:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.text('125% gratuity for parties of 4+')
 
-streamlit.header('👽💩💩 Build Your Own Fruit Smoothie 💩💩👽')
+streamlit.header('👽💩💩 Build Your Own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
