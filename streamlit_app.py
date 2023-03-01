1c01db536aa4d40d434511ae261c9bf26540b7ef
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-01 12:11:55 -0700

    Update streamlit_app.py
    I added some nonsense

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.text('125% gratuity for parties of 4+')
