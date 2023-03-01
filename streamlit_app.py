ca9c50b6604e498af01d1b5edd62085da52584c3
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-01 12:19:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.text('125% gratuity for parties of 4+')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
