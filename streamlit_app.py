169c681c8e067a4ef765379ed38318d4b395b8f5
MikeBurston <126702322+MikeBurston@users.noreply.github.com>
2023-03-01 12:14:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,9 @@ import streamlit
 streamlit.title('I Like cheese')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.text('125% gratuity for parties of 4+')
-streamlit.text('🥣 🥗 🐔 🥑🍞')
-streamlit.text('🥑🍞 Avocado Toast')
